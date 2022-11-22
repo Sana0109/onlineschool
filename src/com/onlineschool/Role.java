b023f3bbc6fddc00d6756d80dda94a9fbad32140
Oksana_Vasilkova <vasilkova010972@gmail.com>
2022-11-22 21:02:59 +0200

    constructor create person

--- src/com/onlineschool/Role.java
+++ src/com/onlineschool/Role.java
@@ -1,5 +1,27 @@
 package com.onlineschool;
 
 public enum Role {
-    STUDENT,TEACHER
+
+    TEACHER("nameTeacher1") ;
+
+
+        private String nameTeacher;
+
+        Role(String nameTeacher) {
+            this.nameTeacher = nameTeacher;
+        }
+
+/*STUDENT("nameStudent");
+        private String nameStudent;
+    Role(String nameStudent){
+        this.nameStudent = nameStudent;
+    }
+*/
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "nameTeacher='" + nameTeacher + '\'' +
+                '}';
+    }
 }
--- src/com/onlineschool/models/Person.java
+++ src/com/onlineschool/models/Person.java
@@ -2,5 +2,12 @@ package com.onlineschool.models;
 
 public class Person {
     private int ID;
+    private int courseID;
     private  String role;
+
+    public Person(int ID,int courseID,String role){
+        this.ID = ID;
+        this.courseID = courseID;
+        this.role = role;
+    }
 }
