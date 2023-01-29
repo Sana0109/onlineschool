67b3f08d30923fe29d3d28460fa999f9d6817b70
Oksana_Vasilkova <vasilkova010972@gmail.com>
2023-01-29 16:13:33 +0200

    added class Log

--- src/com/onlineschool/models/Log.java
+++ src/com/onlineschool/models/Log.java
@@ -2,5 +2,67 @@ package com.onlineschool.models;
 
 public class Log {
     private String name;
+    private LevelLog level;
+    private String message;
+    private Integer localDateTime;
+    private String stackTrace;
 
+    public Log(String name, LevelLog level, String message, Integer localDateTime, String stackTrace) {
+        this.name = name;
+        this.level = level;
+        this.message = message;
+        this.localDateTime = localDateTime;
+        this.stackTrace = stackTrace;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LevelLog getLevel() {
+        return level;
+    }
+
+    public void setLevel(LevelLog level) {
+        this.level = level;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(Integer localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public String getStackTrace() {
+        return stackTrace;
+    }
+
+    public void setStackTrace(String stackTrace) {
+        this.stackTrace = stackTrace;
+    }
+
+    @Override
+    public String toString() {
+        return "Log{" +
+                "name='" + name + '\'' +
+                ", level=" + level +
+                ", message='" + message + '\'' +
+                ", localDateTime=" + localDateTime +
+                ", stackTrace='" + stackTrace + '\'' +
+                '}';
+    }
 }
