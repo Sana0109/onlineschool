7711ee9c0c169c2ffbee7c5f83fc27ccaaabe4b7
Oksana_Vasilkova <vasilkova010972@gmail.com>
2023-02-01 23:55:27 +0200

    checked collection

--- src/com/onlineschool/Main.java
+++ src/com/onlineschool/Main.java
@@ -3,18 +3,34 @@ package com.onlineschool;
 
 
 
-import com.onlineschool.repository.*;
+import com.onlineschool.models.Lecture;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 
 public class Main {
     public static void main(String[] args) {
+//знаходилось в класі LectureRepository
+            List<Lecture> lectureCollectionArray = new ArrayList<>();
+            lectureCollectionArray.add(new Lecture(1));
+            lectureCollectionArray.add(new Lecture(2));
+            lectureCollectionArray.add(new Lecture(3));
+            lectureCollectionArray.add(new Lecture(4));
+            lectureCollectionArray.add(new Lecture(5));
 
+            System.out.println("The whole collection :\n" + lectureCollectionArray);
+            System.out.println("---------------");
 
+            ListIterator<Lecture> simpleIterator = lectureCollectionArray.listIterator(2);
+            if (simpleIterator.hasNext())
+                System.out.println("Choose element :\n" + simpleIterator.next());
+            System.out.println("---------------");
+            simpleIterator.remove();
+            System.out.println("A collection without a deleted element :\n" + lectureCollectionArray);
 
-
-        LectureRepository.findAll();
 
 
 
--- src/com/onlineschool/repository/LectureRepository.java
+++ src/com/onlineschool/repository/LectureRepository.java
@@ -8,25 +8,7 @@ import java.util.*;
 
 public class LectureRepository extends GenericRepository {
 
-    public static void findAll() {
 
-       List<Lecture> lectureCollectionArray = new ArrayList<>();
-        lectureCollectionArray.add(new Lecture(1));
-        lectureCollectionArray.add(new Lecture(2));
-        lectureCollectionArray.add(new Lecture(3));
-        lectureCollectionArray.add(new Lecture(4));
-        lectureCollectionArray.add(new Lecture(5));
-
-        System.out.println("The whole collection :\n" + lectureCollectionArray);
-        System.out.println("---------------");
-
-        ListIterator<Lecture> simpleIterator = lectureCollectionArray.listIterator(2);
-        if (simpleIterator.hasNext())
-            System.out.println("Choose element :\n" + simpleIterator.next());
-        System.out.println("---------------");
-        simpleIterator.remove();
-        System.out.println("A collection without a deleted element :\n" + lectureCollectionArray);
-    }
 
     private final int BOXES = 10;
     private final Lecture[] lectures = new Lecture[BOXES];
